2f341db87e875fc822f82a7ab31b92a4f3642aca
Mohammad Hasan <aamirkhan2009@hotmail.com>
2023-03-25 07:34:42 +0530

    Added header

--- Deliveroo-clone/screens/Home.js
+++ Deliveroo-clone/screens/Home.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {React, useLayoutEffect} from 'react'
+import { useNavigation } from '@react-navigation/native'
+import {AdjustmentsVerticalIcon, MagnifyingGlassIcon, ChevronDownIcon, UserIcon} from "react-native-heroicons/outline"
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [])
+
   return (
-    <View>
-      <Text className="text-red-600">Home</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-5">
+      <Text>
+        {/*hEADER*/}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"/>
+
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <Text className="font-bold text-xl">Current Location
+              <ChevronDownIcon size={20} color="#00CCBB"/>
+            </Text>
+          </View>
+
+          <UserIcon size={35} color="#00CCBB" className="flex-row-reverse"/>
+        </View>
+
+        {/*Search*/}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4">
+          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            <MagnifyingGlassIcon color="gray" size={20}/>
+            <TextInput placeholder="Restaurants and Cuisines" keyboardType='default'/>
+          </View>
+
+          <AdjustmentsVerticalIcon color="#00CCBB"/>
+        </View>
+      </Text>
+    </SafeAreaView>
   )
 }
 
-export default Home
+export default Home;
