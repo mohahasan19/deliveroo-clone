62fc86159dee156fe811bfc59ee4ff103c157445
Mohammad Hasan <aamirkhan2009@hotmail.com>
2023-03-29 15:55:17 +0530

    Added sanity schemas

--- Deliveroo-clone/sanity/sanity.cli.js
+++ Deliveroo-clone/sanity/sanity.cli.js
@@ -1,8 +1,17 @@
 import {defineCliConfig} from 'sanity/cli'
+//import { ImageUrlBuilder } from '@sanity/image-url/lib/types/builder'
 
-export default defineCliConfig({
+const sanityClient = defineCliConfig({
   api: {
     projectId: 'llrfu6uv',
-    dataset: 'production'
+    dataset: 'production',
+    useCdn: true,
+    apiVersion: "2021-10-21"
   }
 })
+
+export default sanityClient;
+/*
+const builder = ImageUrlBuilder(client)
+export const urlFor = (source) => builder.image(source)
+*/
