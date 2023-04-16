c46fcdc41879a60ec05f1f94939320eba1e3d385
Mohammad Hasan <aamirkhan2009@hotmail.com>
2023-04-16 15:57:07 +0530

    Added preparing order screen with animations

--- Deliveroo-clone/App.js
+++ Deliveroo-clone/App.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import BasketScreen from './screens/BasketScreen';
 import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,7 +20,8 @@ export default function App() {
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Restaurant" component={RestaurantScreen}/>
         <Stack.Screen name="Basket" component={BasketScreen} options={{presentation: 'modal', headerShown: false}}/>
-        <Stack.Screen name="PreparingOrder" component={PreparingOrderScreen}/>
+        <Stack.Screen name="PreparingOrder" component={PreparingOrderScreen} options={{presentation: 'fullScreenModal', headerShown: false}}/>
+        <Stack.Screen name="Delivery" component={DeliveryScreen} options={{presentation: 'fullScreenModal', headerShown: false}}/>
       </Stack.Navigator>
     </Provider>
     
--- Deliveroo-clone/screens/PreparingOrderScreen.js
+++ Deliveroo-clone/screens/PreparingOrderScreen.js
@@ -1,11 +1,34 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import * as Animatable from "react-native-animatable"
+import * as Progress from "react-native-progress"
+import { useNavigation } from '@react-navigation/native'
+import { useEffect } from 'react'
 
 const PreparingOrderScreen = () => {
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        setTimeout(()=> {
+            navigation.navigate("Delivery")
+        }, 4000)
+    })
   return (
-    <View>
-      <Text>PreparingOrderScreen</Text>
-    </View>
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center"> 
+        <Animatable.Image
+        source={require("../assets/orderLoding.gif")}
+        animation="slideInUp"
+        className="h-96 w-96"
+        iterationCount={1} />
+        <Animatable.Text
+        animation="slideInUp"
+        iterationCount={1}
+        className="text-lg my-10 text-white font-bold text-center">
+            Waiting for Restaurant to accept your order!    
+        </Animatable.Text>
+        <Progress.Circle size={60} indeterminate={true} color="white" />
+    </SafeAreaView>
   )
 }
 
